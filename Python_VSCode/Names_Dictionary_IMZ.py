4502d521dbb6deba86338b3ba30cae5959f17744
Prakhar Kumar <prakharkumar506978@gmail.com>
2020-04-05 20:05:15 +0530

    05-04-2020

---
+++ Python_VSCode/Names_Dictionary_IMZ.py
@@ -0,0 +1,5 @@
+d = {'names': ['Kevin', 'Will', 'John', 'Daniel', 'Albert'], 'Age': [23, 45, 32, 56, 67]}
+key = input('Enter Key :- ')
+ind = int(input('Enter index :- '))
+print(d[key][ind])
+print(type(d[key]))
