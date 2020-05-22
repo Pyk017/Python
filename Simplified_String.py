882289f535b81175bc6520ef1aeafcc3eb88631c
Prakhar Kumar <prakharkumar506978@gmail.com>
2020-05-22 22:30:26 +0530

    22-05-2020

--- Simplified_String.py
+++ Simplified_String.py
@@ -1,14 +1,14 @@
-# def simplified(s, res = []):
-#     if len(s) == 1:
-#         res.append(s[0])
-#         return res
-#     else:
-#         temp = simplified(s[1:], res)
-#         if s[0] != temp[-1]:
-#             res.append(s[0])
-#         return res
+def simplified(s, res = []):
+    if len(s) == 1:
+        res.append(s[0])
+        return res
+    else:
+        temp = simplified(s[1:], res)
+        if s[0] != temp[-1]:
+            res.append(s[0])
+        return res
 
 
-# string = input()
-# res = ''.join(simplified(string)[::-1])
-# print(res)
+string = input()
+res = ''.join(simplified(string)[::-1])
+print(res)
