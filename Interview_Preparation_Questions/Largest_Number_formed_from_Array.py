71157fc8b851641791116dd57347a7351fb9c2f2
Prakhar Kumar <prakharkumar506978@gmail.com>
2020-06-06 20:52:47 +0530

    06-06-2020

--- Interview_Preparation_Questions/Largest_Number_formed_from_Array.py
+++ Interview_Preparation_Questions/Largest_Number_formed_from_Array.py
@@ -1,4 +1,5 @@
 for _ in range(int(input())):
     n = int(input())
-    ar = sorted(input().split(), reverse=True)
-    print(ar)
+    ar = list(map(int, input().split()))
+    ar = sorted(ar, key=lambda x: str(x)*10, reverse=True)
+    print(''.join([str(i) for i in ar]))
